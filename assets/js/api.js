4af772f84a0ef27b8ff2fe7aa4eb35413942eb7b
yigitsenal <yigitsenal35@gmail.com>
2022-12-05 14:38:12 +0300

    #

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,7 +5,7 @@ var url = 'https://newsapi.org/v2/top-headlines?country=tr&apiKey=538580b6dd0f4d
     var req = new Request(url);
     var news = document.getElementById('news')
     var news2 = document.getElementById('news2_title')
-    var news2_url= document.getElementById('news2_url')
+    //var news2_url= document.getElementById('news2_url')
     var news2_publish = document.getElementById('news2_publish')
     var news2_image = document.getElementById('news2_image')
     var news3 = document.getElementById('news3')
@@ -13,6 +13,7 @@ var url = 'https://newsapi.org/v2/top-headlines?country=tr&apiKey=538580b6dd0f4d
     var news4 = document.getElementById('news4')
     var news4_image = document.getElementById('news4_image')
     var news5 = document.getElementById('news5')
+    var news5_image = document.getElementById('news5_image')
     var news6 = document.getElementById('news6')
     var news6_image = document.getElementById('news6_image')
     var news6_description = document.getElementById('news6_description')
@@ -33,13 +34,14 @@ var url = 'https://newsapi.org/v2/top-headlines?country=tr&apiKey=538580b6dd0f4d
         news.innerHTML = jsonData.articles[0].title
         news2.innerHTML = jsonData.articles[1].title
         news2_publish.innerHTML = jsonData.articles[1].publishedAt
-        news2_url.innerHTML=jsonData.articles[1].url
+        //news2_url.innerHTML=jsonData.articles[1].url
         news2_image.src = jsonData.articles[1].urlToImage
         news3.innerHTML = jsonData.articles[2].title
         news3_image.src = jsonData.articles[2].urlToImage
         news4.innerHTML = jsonData.articles[3].title
         news4_image.src = jsonData.articles[3].urlToImage
         news5.innerHTML = jsonData.articles[4].title
+        news5_image.src = jsonData.articles[4].urlToImage
         news6.innerHTML = jsonData.articles[5].title
         news6_image.src = jsonData.articles[5].urlToImage
         news6_description.innerHTML = jsonData.articles[5].description
